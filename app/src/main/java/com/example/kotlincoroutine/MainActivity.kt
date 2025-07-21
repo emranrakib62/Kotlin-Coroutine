f9d083d51bd5f5ee67947dfe282a50e3aefe4e52
Emranul Hoque <emranrakib01862@gmail.com>
2025-07-21 23:09:49 +0600

    initial commit

---
+++ app/src/main/java/com/example/kotlincoroutine/MainActivity.kt
@@ -0,0 +1,56 @@
+package com.example.kotlincoroutine
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.kotlincoroutine.databinding.ActivityMainBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlin.concurrent.thread
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding:ActivityMainBinding
+    var value=0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding=ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        Log.i("Tag","1.Thread name:${Thread.currentThread()}")
+        binding.increment.setOnClickListener{
+            value++
+            binding.incrementedValue.text="$value"
+        }
+        binding.longtaskbutton.setOnClickListener{
+
+            CoroutineScope(Dispatchers.IO).launch {
+                Log.i("Tag", "2.Thread name:${Thread.currentThread()}")
+                for (i in 0..9999999999999999L){
+
+
+                }
+
+                }
+
+            CoroutineScope(Dispatchers.Default).launch {
+                Log.i("Tag", "3.Thread name:${Thread.currentThread()}")
+                for (i in 0..9999999999999999L){
+
+
+                }
+
+            }
+
+
+
+
+        }
+
+
+    }
+}
